fc952aa80aae26955670534bee60a7649c6798d9
Miranda Steele <miranda@purestorage.com>
2018-07-20 13:13:51 -0700

    Initial commit - adding what I have so far

---
+++ experiments/grid_test.py
@@ -0,0 +1,10 @@
+from Tkinter import *
+
+Label(master, text="First").grid(row=0)
+Label(master, text="Second").grid(row=1)
+
+e1 = Entry(master)
+e2 = Entry(master)
+
+e1.grid(row=0, column=1)
+e2.grid(row=1, column=1)
